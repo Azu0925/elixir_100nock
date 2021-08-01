f17cb57bad9fd90ad8eb1b4edeca602105df58cc
Azu0925 <yamaneko564783@gmail.com>
2021-08-02 03:02:01 +0900

    ちょい修正

--- lib/chapter/one.ex
+++ lib/chapter/one.ex
@@ -4,14 +4,14 @@ defmodule One do
     reverse("", String.split("stressed", "", trim: true))
   end
 
-  def reverse(text, list) do
+  defp reverse(text, list) do
     case list do
       [] ->
-        IO.puts("A. " <> text)
-        IO.puts("\n============================")
+        IO.puts("\nA. " <> text)
+        IO.puts("\n============================\n")
       list ->
-        text = text <> List.last(list)
-        reverse(text, List.delete_at(list, length(list) - 1))
+        joined_text = text <> List.last(list)
+        reverse(joined_text, List.delete_at(list, length(list) - 1))
     end
   end
 end
