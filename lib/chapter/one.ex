8e4ff9a4cf4e901d1144f464936d682c9b880365
Azu0925 <yamaneko564783@gmail.com>
2021-09-09 14:56:40 +0900

    3途中

--- lib/chapter/one.ex
+++ lib/chapter/one.ex
@@ -35,6 +35,11 @@ defmodule One do
     IO.puts "\n=================================\n"
   end
 
+  def three do
+    IO.puts "'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．"
+
+  end
+
   defp reverse(text, list) do
     case list do
       [] ->
